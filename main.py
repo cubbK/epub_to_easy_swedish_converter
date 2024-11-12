e6530e2cd300252f8765157939ac03a29d015a42
Dan <yo@gmail.om>
2024-11-12 18:09:32 +0100

    Add progress logging to HTML processing using tqdm

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pypandoc
 import os
 from bs4 import BeautifulSoup
 import sys
+from tqdm import tqdm
 
 filepath = sys.argv[1]
 print("Starting...")
@@ -33,9 +34,8 @@ with open('./output/index.html') as f:
     descendants_list = list(soup.descendants)
     total_elements = len(descendants_list) - 1
 
-    for index, element in enumerate(descendants_list):
-        print("process block ", index, " of " ,total_elements)
-        if element.name == 'p' or element.name == 'h1' or element.name == 'h2' or element.name == 'h3' or element.name == 'h4' or element.name == 'h5' or element.name == 'h6' or element.name == 'li':
+    for index, element in tqdm(enumerate(descendants_list), total=total_elements, desc="Processing blocks", unit="block"):
+        if element.name in ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li']:
             element.string = "translated text"
 
     pypandoc.convert_text(soup.prettify(), 'epub', format='html', outputfile=f"./output/{filename}_latt_svenska.epub")
