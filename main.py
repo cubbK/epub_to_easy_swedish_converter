86ffc2f4dc04928e9f756174e5f34e96688ec265
Dan <yo@gmail.om>
2024-11-12 22:54:40 +0100

    Integrate GPT-4 model for converting Swedish text to easy Swedish in HTML processing

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from bs4 import BeautifulSoup
 import sys
 from tqdm import tqdm
+from gpt4all import GPT4All
+
+
 
 filepath = sys.argv[1]
 print("Starting...")
@@ -27,16 +30,32 @@ pypandoc.convert_file(filepath,
                  )
 print("Done extracting epub file")
 
+
 with open('./output/index.html') as f:
-    html_doc = f.read()
-    soup = BeautifulSoup(html_doc, 'html.parser')
+        html_doc = f.read()
+        soup = BeautifulSoup(html_doc, 'html.parser')
 
-    descendants_list = list(soup.descendants)
-    total_elements = len(descendants_list) - 1
+        descendants_list = list(soup.descendants)
+        total_elements = len(descendants_list) - 1
 
-    for index, element in tqdm(enumerate(descendants_list), total=total_elements, desc="Processing blocks", unit="block"):
-        if element.name in ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li']:
-            element.string = "translated text"
 
-    pypandoc.convert_text(soup.prettify(), 'epub', format='html', outputfile=f"./output/{filename}_latt_svenska.epub")
-    print("Done writing new book to ", f"./output/{filename}_latt_svenska.epub")
+
+        for index, element in tqdm(enumerate(descendants_list), total=total_elements, desc="Processing blocks", unit="block"):
+            
+            if element.name in ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li']:
+                if(len(element.text) == 0) :
+                    continue
+                if(len(element.text) < 30) :
+                    continue
+
+                print("Processing: ", element.text, index)
+                model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf") # downloads / loads a 4.66GB LLM
+                with model.chat_session():
+                    new_text = model.generate(f"with the following text in quotes: '{element.text}' which is in swedish. Convert it to easy swedish that can be understood at A2 level. Output ONLY the converted text and nothing more! If the text cannot be converted output the original. DO NOT add any comments. remove '. speak in SWEDISH.", max_tokens=2048)
+                    element.string = new_text + "  CONVERTED " + "ORIGINAL: " + element.text
+        
+        with open(f"./output/{filename}_latt_svenska.html", 'w') as output:
+            output.write(soup.prettify())
+
+        pypandoc.convert_text(soup.prettify(), 'epub', format='html', outputfile=f"./output/{filename}_latt_svenska.epub")
+        print("Done writing new book to ", f"./output/{filename}_latt_svenska.epub")
--- readme.md
+++ readme.md
@@ -2,9 +2,30 @@
 
 Convert your books to easy swedish with the help of LLMs
 
-Supports both _swedish_ to _easy swedish_ conversions and _english_ to _easy swedish_ conversions.
+Supports _swedish_ to _easy swedish_ conversions.
 
-Adaptable to any language.
+## Example
+
+Original:
+
+> ...Han gick ut från brandstationen och längs den nattliga gatan mot tunnelbanan, där det tysta luftdrivna tåget gled ljudlöst fram på sin oljade bana nere i jorden och med en kraftig puff av varm luft knuffade ut honom mot rulltrappan av gräddfärgat tegel som ledde upp till förstaden. ...
+
+Converted:
+
+> ...Han gick ut från brandstationen och längs en mörk gata mot tunnelbanan, där tåget rullade fram på sin bana i jorden. När han kom upp till trappan ledde den honom upp till förstaden. ...
+
+## Getting Started
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 main.py ./example.epub
+```
+
+The conversion on my m1 pro takes
+
+- ~100 minutes on the book with 45000 words(average book size) `example.epub`
 
 ## Why?
 
@@ -14,16 +35,4 @@ I want to read swedish books. Unfortunatelly I'm not at the level that I can und
 
 It converts the epub to html with pandadoc and then for every paragraph it runs an LLM prompt with https://docs.gpt4all.io/index.html and bundles the modified content back into .epub.
 
-## Getting Started
-
-TODO
-
-## Run Source
-
-```
-pip install -r requirements.txt
-```
-
-```
-python3 main.py ./example.epub
-```
+It uses the default Meta-Llama-3-8B-Instruct.Q4_0 that requires 8gb or ram and is not very fast but any model supported by https://docs.gpt4all.io/ is possible.
