99a01d553b468e0e3fd3172c18753f55c427713b
Dan <yo@gmail.om>
2024-11-12 17:22:17 +0100

    Add initial implementation for EPUB to HTML conversion and translation

---
+++ main.py
@@ -0,0 +1,32 @@
+import pypandoc
+import os
+from bs4 import BeautifulSoup
+
+filepath = "/Users/dan.popa/work/epub_to_latt_svenska_converter/ebook2.epub"
+pathname, filename = os.path.split(filepath)
+targetfilename = 'index.html'
+
+
+pypandoc.convert_file(filepath,
+                  format='epub',
+                  to='html5',
+                  extra_args=[
+                      '--read=epub',
+                      f'--extract-media={pathname}/output',
+                      '--wrap=none',
+                      '--standalone',
+                  ],
+                  encoding='utf-8',
+                  outputfile=pathname + '/output/' + targetfilename,
+                  filters=None,
+                  verify_format=True
+                 )
+
+with open('./output/index.html') as f:
+    html_doc = f.read()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    for element in soup.descendants:
+        if element.name == 'p' or element.name == 'h1' or element.name == 'h2' or element.name == 'h3' or element.name == 'h4' or element.name == 'h5' or element.name == 'h6' or element.name == 'li':
+            element.string = "translated text"
+
+    pypandoc.convert_text(soup.prettify(), 'epub', format='html', outputfile="./output/translated.epub")
